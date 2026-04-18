7567cbe78086ba8130b736ecfcecd79a4c9774fe
minjiazhang83-web <minjiazhang83@gmail.com>
2026-04-18 20:12:10 +0000

    f1

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,9 +11,9 @@ client = OpenAI(api_key=st.secrets["key"])
 
 system_prompt = "You are a translator. REspond with the sentence, phrase, or word that the user wants you to translate only."
 
+lang = st.text_input('What language do you want to translate to?')
 
 with st.form("language"):
-    lang = st.text_input('What language do you want to translate to?')
 
     text = st.text_input(f"What text do you want to be translated to {lang}")
 
