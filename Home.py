3f0b75e39ca4fe08c13691198878748369807f01
minjiazhang83-web <minjiazhang83@gmail.com>
2026-04-25 20:10:49 +0000

    test1

--- Home.py
+++ Home.py
@@ -5,18 +5,18 @@ You can stop looking at this now.
 
 Please.
 """
-if 'tama' not in st.session_state:
-    st.session_state['tama']= {'Name':'pablo',"type":"monkey",'hunger':0,'thirst':0,'boredness':5}
-st.image("image.png")
 
-feed = st.button('Feed tamagotchi')
-drink = st.button('Give water to tamagotchi')
-if 'number' not in st.session_state:
+types = ['turtle','duck','fish','egg','rabbit']
+with st.form("make-tama"):
+    tama_type = st.selectbox("Select Tamagotchi type",types)
 
-    st.session_state['number'] = 0
+    name = st.text_input("Enter name here")
 
-clicked_button = st.button("Press me!")
-if clicked_button:
-    st.session_state['number'] += 1
+    submit = st.form_submit_button("Create and play!")
 
-st.write(st.session_state['number'])
+    if submit:
+
+        if 'tama' not in st.session_state:
+            st.session_state['tama']= {'Name':name,"type":tama_type,'hunger':0,'thirst':0,'boredness':5}
+
+
--- play.py
+++ play.py
@@ -1,5 +1,12 @@
 import streamlit as st
 
 st.set_page_config(page_title="Tamagotchi",page_icon="🐵")
+st.image("image.png")
 
-"Hello world!"
+st.session_state['tama']['name']
+"is your faithful " + st.session_state['tama']['type']
+"their hunger is " + st.session_state['tama']['hunger'] + "and thirst is " + st.ession_state['tama']['thirst']
+"their boredness is " + st.session_State['tama']['boredness']
+
+
+feed = st.button('Feed tamagotchi')
