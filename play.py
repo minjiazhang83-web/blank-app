77a31430d2503930ae59d517d4d79e5453713d5c
minjiazhang83-web <minjiazhang83@gmail.com>
2026-04-25 20:23:57 +0000

    data

--- play.py
+++ play.py
@@ -2,16 +2,11 @@ import streamlit as st
 
 st.set_page_config(page_title="Tamagotchi",page_icon="🐵")
 st.image("image.png")
+st.write(f""" 
+{st.session_state['tama']['name']} is your faithful {st.session_state['tama']['type']} tamagotchi!
+their hunger is {st.session_state['tama']['hunger']} and thirst is {st.session_state['tama']['thirst']}
+their boredness is {st.session_state['tama']['boredness']}
 
-st.session_state['tama']['name']
-"is your faithful "
-st.session_state['tama']['type']
-"their hunger is "
-st.session_state['tama']['hunger']
-"and thirst is "
-st.ession_state['tama']['thirst']
-"their boredness is "
-st.session_State['tama']['boredness']
-
+""")
 
 feed = st.button('Feed tamagotchi')
