b21483cf1263bc0315074a645ae70120d2a8129d
minjiazhang83-web <minjiazhang83@gmail.com>
2026-04-25 20:21:29 +0000

    streamlit1

--- play.py
+++ play.py
@@ -4,9 +4,14 @@ st.set_page_config(page_title="Tamagotchi",page_icon="🐵")
 st.image("image.png")
 
 st.session_state['tama']['name']
-"is your faithful " + st.session_state['tama']['type']
-"their hunger is " + st.session_state['tama']['hunger'] + "and thirst is " + st.ession_state['tama']['thirst']
-"their boredness is " + st.session_State['tama']['boredness']
+"is your faithful "
+st.session_state['tama']['type']
+"their hunger is "
+st.session_state['tama']['hunger']
+"and thirst is "
+st.ession_state['tama']['thirst']
+"their boredness is "
+st.session_State['tama']['boredness']
 
 
 feed = st.button('Feed tamagotchi')
