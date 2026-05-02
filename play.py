be6d7f619d77fa12f71d80d0435170604a389108
minjiazhang83-web <minjiazhang83@gmail.com>
2026-05-02 20:18:05 +0000

    events

--- play.py
+++ play.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import json
 from openai import OpenAI
+import random
 
 
 if('play' not in st.session_state):
@@ -50,27 +51,55 @@ if event:
     # 2
     system_prompt = """
     You are running a virtual pet tamagotchi. This tamagotchi has 3 different stats: hunger, thirst, and boredness.
-      each time a user clicks a button you will generate a new event, make 35% of the events good, 30% ok, 30% bad, 5% catastrophic. for each event return a new json object of how the stats changed as well as a description:
-
+      each time a user clicks a button you will generate a new event, the event will be of different levels of how important it is. those will be given by the user
 
 
   {'name':name,"type":tama_type,'hunger':0,'thirst':0,'boredness':5,
-    "event": "event description"}
+    "event": "event description"} , good
         
 
     here are some examples:
 
     user starting data: {'name':test,"type":"turtle",'hunger':3,'thirst':2,'boredness':6,
-    "event":"Welcome and good luck taking care of your pet!"}
+    "event":"Welcome and good luck taking care of your pet!"} , good
 
     assistant response (good): 
-        {'name':test,"type":turtle,'hunger':5,'thirst':5,'boredness':4,
-          "event": "test the turtle goes for a long swim, this makes them tired and thirsty but less bored."             }
+        {'name':test,"type":turtle,'hunger':5,'thirst':5,'boredness':3,
+          "event": "test the turtle goes for a long swim, this makes them tired and thirsty but much less bored."             }
    
+    user starting data: {'name':test,"type":"turtle",'hunger':3,'thirst':2,'boredness':6,
+    "event":"Welcome and good luck taking care of your pet!"} , bad
+
+    assistant response (bad): 
+        {'name':test,"type":turtle,'hunger':7,'thirst':5,'boredness':8,
+          "event": "you eat plastic on accident, this hurts you making you sick, making you very hungry and thirsty."             }
+   
+    user starting data: {'name':test,"type":"turtle",'hunger':1,'thirst':4,'boredness':5,
+    "event":"Welcome and good luck taking care of your pet!"} , ok
+
+    assistant response (ok): 
+        {'name':test,"type":turtle,'hunger':2,'thirst':5,'boredness':4,
+          "event": "test the turtle takes a nap, making them slgihtly less bored, and slightly more hungry and thirsty" }
+    
+    user starting data: {'name':test,"type":"turtle",'hunger':3,'thirst':2,'boredness':6,
+    "event":"Welcome and good luck taking care of your pet!"} , catastrophic
+
+    assistant response (catastrophic): 
+        {'name':"test the dead rabbit","type":rabbit,'hunger':None,'thirst':None,'boredness':None,
+          "event": "your rabbit went into a bath and came under hypothermic shock, unfortunately passing away"             }
    
     """
 
-    
+    roll = random.randint(1,100)
+    event_type = ''
+    if roll < 45:
+        event_type = ', good'
+    elif roll < 65:
+        event_type = ', ok'
+    elif roll < 95:
+        event_type = ", bad"
+    else:
+        event_type = ', catastrophic'
     # 3
     user_prompt = json.dumps(st.session_state['tama'])
 
