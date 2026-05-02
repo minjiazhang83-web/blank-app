b75d3ef4551a4c6a68ae397f822589e10a7af839
minjiazhang83-web <minjiazhang83@gmail.com>
2026-05-02 19:43:58 +0000

    test

--- play.py
+++ play.py
@@ -21,52 +21,70 @@ drink = st.button("Give water to tamagotchi")
 play = st.button("Play with tamagotchi")
 wash = st.button("Wash your tamagotchi")
 
-if feed and st.session_state['tama']['hunger'] >=0:
+if feed and st.session_state['tama']['hunger'] >0:
     st.session_state['tama']['hunger'] -=1
     
-if drink and st.session_state['tama']['thirst'] >=0:
+if drink and st.session_state['tama']['thirst'] >0:
     st.session_state['tama']['thirst'] -=1
     
-if play and st.session_state['tama']['boredness'] >=0:
+if play and st.session_state['tama']['boredness'] >0:
     st.session_state['tama']['boredness'] -=1
 
-if wash and st.session_state['tama']['hunger'] <11:
+if wash and st.session_state['tama']['boredness'] <11:
     st.session_state['tama']['boredness'] +=1
 
 
     # 1
 
-client = OpenAI(
-    api_key = st.secrets("key")
-)
 
-# 2
-system_prompt = """
-You are a dungeon master for a DND game. A user will give you a brief description of the world they are playing in. 
+event = st.button("Generate random event")
 
-Given a description of the fantasy world, come up with a name and description for each party member, and respond with a JSON in the following format:
+if event:
+        
 
-{
-    "NAME_1": "DESCRIPTION_1",
-    "NAME_2": "DESCRIPTION_2"
-}
-"""
+    client = OpenAI(
+        api_key = st.secrets("key")
+    )
 
-""""""
-# 3
-user_prompt = input("Describe the world that you will be playing in.")
+    # 2
+    system_prompt = """
+    You are running a virtual pet tamagotchi. This tamagotchi has 3 different stats: hunger, thirst, and boredness. each time a user clicks a button you will generate a new event, make 60% of the events good, 20% ok, 10% bad, 10% catastrophic. for each event return a new json object of how the stats changed as well as a description:
 
 
-# 4
-response = client.chat.completions.create(
-    model="gpt-4o",
-    response_format={ "type": "json_object" },
-    messages=[
-        {"role": "system", "content": system_prompt},
-        {"role": "user", "content": user_prompt}
-    ]
-)
+    {
+        "tama": {'name':name,"type":tama_type,'hunger':0,'thirst':0,'boredness':5},
+        "event": "event description"
+    }
 
-# 5
-#(json.loads(response.choices[0].message.content))
-""""""
+    here are some examples:
+
+    user starting data: {'name':test,"type":"turtle",'hunger':3,'thirst':2,'boredness':6}
+
+    assistant response: 
+        {
+        "tama": {'name':test,"type":turtle,'hunger':5,'thirst':5,'boredness':4},
+        "event": "test the turtle goes for a long swim, this makes them tired and thirsty but less bored."
+    }
+    """
+
+    
+    # 3
+    user_prompt = st.session_state['tama']
+
+
+    # 4
+    response = client.chat.completions.create(
+        model="gpt-4o",
+        response_format={ "type": "json_object" },
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_prompt}
+        ]
+    )
+
+    # 5
+    response = (json.loads(response.choices[0].message.content))
+    st.session_state['tama'] = response['tama']
+    response['event']
+    
+    
